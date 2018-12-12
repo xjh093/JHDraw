26e93c07919117015023bd772a8622da2d9196a4
HaoCold <xjh093@126.com>
2018-12-12 10:22:53 +0800

    Add files via upload

---
+++ JHDraw/UIView+JHDrawCategory.h
@@ -0,0 +1,64 @@
+//
+//  UIView+JHDrawCategory.h
+//  JHKit
+//
+//  Created by Lightech on 14-10-16.
+//  Copyright (c) 2014年 Lightech. All rights reserved.
+//
+//  MIT License
+//
+//  Copyright (c) 2017 xjh093
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (JHDrawCategory)
+
+/// draw line in view.
+- (void)jh_drawLineFromPoint:(CGPoint)fPoint
+                     toPoint:(CGPoint)tPoint
+                   lineColor:(UIColor *)color
+                  lineHeight:(CGFloat)height;
+
+/// draw dash line in view. type: 0 - cube, 1 - round
+- (void)jh_drawDashLineFromPoint:(CGPoint)fPoint
+                         toPoint:(CGPoint)tPoint
+                       lineColor:(UIColor *)color
+                       lineWidth:(CGFloat)width
+                      lineHeight:(CGFloat)height
+                       lineSpace:(CGFloat)space
+                        lineType:(NSInteger)type;
+
+/// draw pentagram in view. rate: 0.3 ~ 1.1
+- (void)jh_drawPentagram:(CGPoint)center
+                  radius:(CGFloat)radius
+                   color:(UIColor *)color
+                    rate:(CGFloat)rate;
+
+/// draw rect. type: 0 - cube, 1 - round
+- (void)jh_drawRect:(CGRect)rect
+          lineColor:(UIColor *)color
+          lineWidth:(CGFloat)width
+         lineHeight:(CGFloat)height
+           lineType:(NSInteger)type
+             isDash:(BOOL)dash
+          lineSpace:(CGFloat)space;
+
+@end
---
+++ JHDraw/UIView+JHDrawCategory.m
@@ -0,0 +1,162 @@
+//
+//  UIView+JHDrawCategory.m
+//  JHKit
+//
+//  Created by Lightech on 14-10-16.
+//  Copyright (c) 2014年 Lightech. All rights reserved.
+//
+//  MIT License
+//
+//  Copyright (c) 2017 xjh093
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+#import "UIView+JHDrawCategory.h"
+
+@implementation UIView (JHDrawCategory)
+
+//画直线 - draw line in view.
+- (void)jh_drawLineFromPoint:(CGPoint)fPoint
+                     toPoint:(CGPoint)tPoint
+                   lineColor:(UIColor *)color
+                   lineHeight:(CGFloat)height
+{
+    CAShapeLayer* shapeLayer = [CAShapeLayer layer];
+    shapeLayer.strokeColor = [UIColor lightGrayColor].CGColor;
+    if (color) {
+        shapeLayer.strokeColor = color.CGColor;
+    }
+    shapeLayer.fillColor = [UIColor clearColor].CGColor;
+    shapeLayer.path = ({
+        UIBezierPath *path = [UIBezierPath bezierPath];
+        [path moveToPoint:fPoint];
+        [path addLineToPoint:tPoint];
+        path.CGPath;
+    });
+    shapeLayer.lineWidth = height;
+    [self.layer addSublayer:shapeLayer];
+}
+
+//画虚线 - draw dash line.
+- (void)jh_drawDashLineFromPoint:(CGPoint)fPoint
+                         toPoint:(CGPoint)tPoint
+                       lineColor:(UIColor *)color
+                       lineWidth:(CGFloat)width
+                      lineHeight:(CGFloat)height
+                       lineSpace:(CGFloat)space
+                        lineType:(NSInteger)type
+{
+    CAShapeLayer* shapeLayer = [CAShapeLayer layer];
+    shapeLayer.strokeColor = [UIColor lightGrayColor].CGColor;
+    if (color) {
+        shapeLayer.strokeColor = color.CGColor;
+    }
+    shapeLayer.fillColor = [UIColor clearColor].CGColor;
+    shapeLayer.path = ({
+        UIBezierPath *path = [UIBezierPath bezierPath];
+        [path moveToPoint:fPoint];
+        [path addLineToPoint:tPoint];
+        path.CGPath;
+    });
+    //第一格虚线缩进多少 - the degree of indent of the first cell
+    //shapeLayer.lineDashPhase = 4;
+    shapeLayer.lineWidth = height < 0 ? 1 : height;
+    shapeLayer.lineCap = kCALineCapButt;
+    width = width < 0 ? 1 : width;
+    shapeLayer.lineDashPattern = @[@(width),@(space)];
+    if (type == 1) {
+        shapeLayer.lineCap = kCALineCapRound;
+        shapeLayer.lineDashPattern = @[@(width),@(space+width)];
+    }
+    [self.layer addSublayer:shapeLayer];
+}
+
+- (void)jh_drawPentagram:(CGPoint)center
+                  radius:(CGFloat)radius
+                   color:(UIColor *)color
+                    rate:(CGFloat)rate
+{
+    CAShapeLayer* shapeLayer = [CAShapeLayer layer];
+    shapeLayer.strokeColor = [UIColor clearColor].CGColor;
+    shapeLayer.fillColor = [UIColor orangeColor].CGColor;
+    if (color) {
+        shapeLayer.fillColor = color.CGColor;
+    }
+    shapeLayer.path = ({
+        UIBezierPath *path = [UIBezierPath bezierPath];
+        
+        //五角星最上面的点
+        CGPoint first  = CGPointMake(center.x, center.y-radius);
+        
+        [path moveToPoint:first];
+        
+        //点与点之间点夹角为2*M_PI/5.0,要隔一个点才连线
+        CGFloat angle=4*M_PI/5.0;
+        if (rate > 1.5) {
+            rate = 1.5;
+        }
+        for (int i= 1; i <= 5; i++) {
+            CGFloat x = center.x - sinf(i*angle)*radius;
+            CGFloat y = center.y - cosf(i*angle)*radius;
+            
+            CGFloat midx = center.x - sinf(i*angle-2*M_PI/5.0)*radius*rate;
+            CGFloat midy = center.y - cosf(i*angle-2*M_PI/5.0)*radius*rate;
+            [path addQuadCurveToPoint:CGPointMake(x, y) controlPoint:CGPointMake(midx, midy)];
+        }
+        
+        path.CGPath;
+    });
+    shapeLayer.lineWidth = 1.0f;
+    shapeLayer.lineJoin = kCALineJoinRound;
+    [self.layer addSublayer:shapeLayer];
+}
+
+- (void)jh_drawRect:(CGRect)rect
+          lineColor:(UIColor *)color
+          lineWidth:(CGFloat)width
+         lineHeight:(CGFloat)height
+           lineType:(NSInteger)type
+             isDash:(BOOL)dash
+          lineSpace:(CGFloat)space
+{
+    CAShapeLayer* shapeLayer = [CAShapeLayer layer];
+    shapeLayer.strokeColor = [UIColor lightGrayColor].CGColor;
+    if (color) {
+        shapeLayer.strokeColor = color.CGColor;
+    }
+    shapeLayer.fillColor = [UIColor clearColor].CGColor;
+    shapeLayer.path = [UIBezierPath bezierPathWithRect:rect].CGPath;
+    shapeLayer.lineWidth = height < 0 ? 1 : height;
+    shapeLayer.lineCap = kCALineCapButt;
+    if (type == 1) {
+        shapeLayer.lineCap = kCALineCapRound;
+    }
+    
+    width = width < 0 ? 1 : width;
+    if (dash) {
+        shapeLayer.lineDashPattern = @[@(width),@(space)];
+        if (type == 1) {
+            shapeLayer.lineDashPattern = @[@(width),@(space+width)];
+        }
+    }
+    
+    [self.layer addSublayer:shapeLayer];
+}
+
+@end
